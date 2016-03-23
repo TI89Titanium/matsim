b96fc5ac65713124695c6ae1a0f2313e96d3b531
Maximilian Wolf <maximilian@wolfs.de>
2016-03-23 11:01:16 +0100

    adding CreateConfig.java to create fullConfig file

---
+++ MATSimTest/src/test/CreateConfig.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.matsim.run.*;
+
+public class CreateConfig {
+
+	public static void main(String[] args) {
+		
+		String path[] = {"C:\\Users\\Maximilian\\Documents\\GitHub\\matsim\\MATSimTest\\input\\fullConfig2.xml"};
+	//	CreateFullConfig config = new CreateFullConfig();
+		CreateFullConfig.main(path);
+	}
+
+}
