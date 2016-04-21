574bcc5b198788bca491658cdd67146b00ac0ee2
Maximilian Wolf <maximilian@wolfs.de>
2016-04-21 19:10:27 +0200

    update controler berlin szenario (plans, network,)
    last iteration 0

--- MATSimBerlin/src/BerlinControler.java
+++ MATSimBerlin/src/BerlinControler.java
@@ -1,9 +1,17 @@
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.Controler;
 
 public class BerlinControler {
 
 	public static void main(String[] args) {
-		Controler controler = new Controler("input/config_be_1pct.xml");
+		Config config = ConfigUtils.loadConfig("input/config_be_1pct.xml");
+		config.network().setInputFile("input/network.xml.gz");
+		config.plans().setInputFile("input/run_160.150.plans_selected.xml.gz");
+		config.controler().setLastIteration(0);
+		
+		config.controler().setOutputDirectory("output/");
+		Controler controler = new Controler(config);
 		controler.run();
 	}
 
