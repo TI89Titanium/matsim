124d484cb8a4efcca9c7f9db441c7f52acfaed35
Maximilian Wolf <maximilian@wolfs.de>
2016-07-23 10:57:55 +0200

    add new leg scoring function

---
+++ MATSimUtilityFunction/utilityfunction/utilityfunction/scoring/function/WolfLegScoring.java
@@ -0,0 +1,32 @@
+package utilityfunction.scoring.function;
+
+import org.matsim.api.core.v01.events.Event;
+import org.matsim.api.core.v01.population.Leg;
+
+public class WolfLegScoring implements org.matsim.core.scoring.SumScoringFunction.LegScoring, org.matsim.core.scoring.SumScoringFunction.ArbitraryEventScoring {
+
+	@Override
+	public void finish() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double getScore() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void handleEvent(Event event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void handleLeg(Leg leg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
